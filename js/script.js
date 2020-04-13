376acc847a73b969dd47ecfeff85ecf5ca1090fd
hidri <agthidri2400@gmail.com>
2020-04-13 20:47:45 +0300

    this commit will add the javascript

---
+++ js/script.js
@@ -0,0 +1,54 @@
+
+
+
+
+var century, month , year,day,gender
+var week = ['Friday','Saturday','Sunday','Monday','TUesday','Wednesday','Thursday'];
+var guyName = ['ንፉዕ','በሊሕ','ሕጉስ','ኣንበሳ','ፈቃር','ጻዕራም','ዓቃል'];
+var ladyName = ['ለዋህ','ጽብቅቲ','ትሕቲ','ጅግና','ሕጉስቲ','ፈቃር','ዓቃል'];
+
+function getdata(){
+    this.century = document.getElementById("century").value;
+    this.month = document.getElementById("month").value;
+    this.year=document.getElementById('year').value;
+    this.day=document.getElementById("")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
